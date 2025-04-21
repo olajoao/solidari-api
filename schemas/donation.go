c478dfa3e840a37252a0dd2103c5d75ee548acca
João Kuster <kusterzito@gmail.com>
2025-04-21 15:09:24 -0300

    feat: donation response struct

--- schemas/donation.go
+++ schemas/donation.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -10,4 +12,15 @@ type Donation struct {
 	Description string
 	Avatar      string
 	Location    string
+}
+
+type DonationResponse struct {
+	ID          uint      `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `json:"deleted_at,omitempty"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Avatar      string    `json:"avatar"`
+	Location    string    `json:"location"`
 }
